35da26b3c519ecb089def5ce9fe015cc3b8a675f
alumno-dam <guillermogarciaf@gmail.com>
2017-10-04 10:31:35 +0200

    Ejercicios Tema 3

---
+++ EjerciciosTema3/Ejercicio05.java
@@ -0,0 +1,19 @@
+/*
+ * Resolución Ejercicio 5 Tema 3
+ * 
+ *
+ * 
+ * 
+ * @author Guillermo García
+ * 
+ */
+import java.util.Scanner;
+
+public class Ejercicio05 {
+  public static void main (String args[]){
+   
+   Scanner s = new Scanner(System.in); 
+   
+   
+  }
+}
