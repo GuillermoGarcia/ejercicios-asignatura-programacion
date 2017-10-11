34d2cdcc165e83990823376625b8d78f9bf1e1c0
Francisca <francisca@portatilpc>
2017-10-11 17:58:10 +0200

    Arreglar Ejercicio 10

--- EjerciciosTema4/Ejercicio10.java
+++ EjerciciosTema4/Ejercicio10.java
@@ -25,29 +25,29 @@ public class Ejercicio10 {
     
     switch(mes){
       case "Enero": if(dia <= 19){ horoscopo = "Capricornio"; }
-                    if (dia > 19){ horoscopo = "Acuario"; } break;
+                    else { horoscopo = "Acuario"; } break;
       case "Febrero": if(dia <= 18){ horoscopo = "Acuario"; }
-                      if (dia > 18){ horoscopo = "Piscis"; } break;
+                      else { horoscopo = "Piscis"; } break;
       case "Marzo": if(dia <= 20){ horoscopo = "Piscis"; }
-                    if (dia > 20){ horoscopo = "Aries"; } break;
+                    else { horoscopo = "Aries"; } break;
       case "Abril": if(dia <= 20){ horoscopo = "Aries"; }
-                    if (dia > 20){ horoscopo = "Tauro"; } break;
+                    else { horoscopo = "Tauro"; } break;
       case "Mayo": if(dia <= 20){ horoscopo = "Tauro"; }
-                    if (dia > 20){ horoscopo = "Géminis"; } break;
+                    else { horoscopo = "Géminis"; } break;
       case "Junio": if(dia <= 20){ horoscopo = "Géminis"; }
-                    if (dia > 20){ horoscopo = "Cáncer"; } break;
+                    else { horoscopo = "Cáncer"; } break;
       case "Julio": if(dia <= 20){ horoscopo = "Cáncer"; }
-                    if (dia > 20){ horoscopo = "Leo"; } break;
+                    else { horoscopo = "Leo"; } break;
       case "Agosto": if(dia <= 21){ horoscopo = "Leo"; }
-                    if (dia > 21){ horoscopo = "Virgo"; } break;
+                     else { horoscopo = "Virgo"; } break;
       case "Septiembre": if(dia <= 22){ horoscopo = "Virgo"; }
-                         if (dia > 22){ horoscopo = "Libra"; } break;
+                         else { horoscopo = "Libra"; } break;
       case "Octubre": if(dia <= 22){ horoscopo = "Libra"; }
-                      if (dia > 22){ horoscopo = "Escorpio"; } break;
+                      else { horoscopo = "Escorpio"; } break;
       case "Noviembre": if(dia <= 22){ horoscopo = "Escorpio"; }
-                        if (dia > 22){ horoscopo = "Sagitario"; } break;
+                        else { horoscopo = "Sagitario"; } break;
       case "Dicimebre": if(dia <= 20){ horoscopo = "Sagitario"; }
-                        if (dia > 20){ horoscopo = "Capricornio"; } break;
+                        else { horoscopo = "Capricornio"; } break;
       default: error=1;
     }
     
