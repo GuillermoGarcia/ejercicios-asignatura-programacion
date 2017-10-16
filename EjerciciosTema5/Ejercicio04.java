3dbd4f6498b8aae068f78df40d95426318673f8e
alumno-dam <guillermogarciaf@gmail.com>
2017-10-16 08:43:56 +0200

    Añadido Ejercicio 4 Tema 5

---
+++ EjerciciosTema5/Ejercicio04.java
@@ -0,0 +1,17 @@
+/*
+ * Solución Ejercicio 4 Tema 5
+ * 
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle 'do-while'.
+ *  
+ * @author Guillermo García
+ */
+
+public class Ejercicio04 {
+  public static void main (String[] args){
+    
+    for (int i=320;i>=160;i=i-20){
+      System.out.printf("Número: %s.\n",i);
+    }
+    
+  }
+}
