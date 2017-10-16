15dea486bfd3b73cf0c54c12ac720d247ac04149
alumno-dam <guillermogarciaf@gmail.com>
2017-10-16 09:35:46 +0200

    Modificado Ejercicio 1 Tema 4

--- EjerciciosTema4/Ejercicio01.java
+++ EjerciciosTema4/Ejercicio01.java
@@ -23,11 +23,11 @@ public class Ejercicio01 {
     
     switch(dia){
       case "Lunes": asignatura = "Programación"; error = 0; break;
-      case "Martes": asignatura = "Sistemas Informaticos"; error = 0;  break;
+      case "Martes": asignatura = "Sistemas Informaticos"; error = 0; break;
       case "Miércoles": 
-      case "Miercoles": asignatura = "Programación"; error = 0;  break;
-      case "Jueves": asignatura = "Entornos de Datos"; error = 0;  break;
-      case "Viernes": asignatura = "Sistemas Informaticos"; error = 0;  break;
+      case "Miercoles": asignatura = "Programación"; error = 0; break;
+      case "Jueves": asignatura = "Entornos de Datos"; error = 0; break;
+      case "Viernes": asignatura = "Sistemas Informaticos"; error = 0; break;
       default: error = 1; asignatura = "Ha introducido algo que no es un día, o bien, un día no hábil.";
     }
     
