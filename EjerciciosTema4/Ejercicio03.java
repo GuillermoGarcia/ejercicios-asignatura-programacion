dab571bf17bf638a709cc508187286e9b4ba5b08
alumno-dam <guillermogarciaf@gmail.com>
2017-10-16 09:43:54 +0200

    Modificado Ejercicio 3 Tema 4

--- EjerciciosTema4/Ejercicio03.java
+++ EjerciciosTema4/Ejercicio03.java
@@ -2,7 +2,7 @@
  * Solución Ejercicio 3 Tema 4
  * 
  * Escribe un programa en que dado un número del 1 a 7 escriba el correspondiente
- *  nombre del día de la semana.
+ * nombre del día de la semana.
  * 
  * @author Guillermo García
  */
