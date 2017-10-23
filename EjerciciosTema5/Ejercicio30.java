fc8e2a7da58d777664e27ff1ba4e2f7209e9a51c
Guillermo García Fernández <guillermogarciaf@gmail.com>
2017-10-23 18:15:43 +0200

    Modificado Ejercicio 30 Tema 5

--- EjerciciosTema5/Ejercicio30.java
+++ EjerciciosTema5/Ejercicio30.java
@@ -18,55 +18,72 @@ public class Ejercicio30 {
 
     long factorial = 1;
     int primerDia = 0;
+    String priDia = "";
     int segundoDia = 0;
+    String segDia = "";
     int primeraHora = 0;
-    boolean primero = true;
+    int segundaHora = 0;
+    boolean correcto = false;
 
-    System.out.println("\033[1mPor favor, Introduzca el primer día (Letras) y la hora.\033[0m");
+    System.out.println("\033[1mPor favor, Introduzca el primer día (Letras) y la hora(0-23).\033[0m");
     do {
-      System.out.println("\033[4mDía: \033[0m");
-      String dia = s.next();
-      boolean correcto = false;
-      switch (dia.toUpperCase()){
-        case "LUNES": primerDia = 1; correcto = true; break;
-        case "MARTES": primerDia = 2; correcto = true; break;
+      System.out.print("\033[4mDía:\033[0m ");
+      priDia = s.next();
+      correcto = false;
+      switch (priDia.toUpperCase()){
+        case "LUNES": primerDia = 1; priDia = "Lunes"; correcto = true; break;
+        case "MARTES": primerDia = 2; priDia = "Martes"; correcto = true; break;
         case "MIÉRCOLES":
-        case "MIERCOLES": primerDia = 3; correcto = true; break;
-        case "JUEVES": primerDia = 4; correcto = true; break;
-        case "VIERNES": primerDia = 5; correcto = true; break;
-        case "SABADO": primerDia = 6; correcto = true; break;
-        case "DOMINGO": primerDia = 7; correcto = true; break;
+        case "MIERCOLES": primerDia = 3; priDia = "Miércoles"; correcto = true; break;
+        case "JUEVES": primerDia = 4; priDia = "Jueves"; correcto = true; break;
+        case "VIERNES": primerDia = 5; priDia = "Viernes"; correcto = true; break;
+        case "SABADO": primerDia = 6; priDia = "Sabado"; correcto = true; break;
+        case "DOMINGO": primerDia = 7; priDia = "Domingo"; correcto = true; break;
       }
       if (!correcto) { System.out.println("Día incorrecto, intentelo de nuevo."); }
     } while (!correcto);
     do {
-      System.out.println("\033[4mHora: \033[0m");
+      System.out.print("\033[4mHora:\033[0m ");
       primeraHora = s.nextInt();
-      if ((primeraHora > 0) && (primeraHora < 23)) { System.out.println("Día incorrecto, intentelo de nuevo."); }
-    } while ((primeraHora > 0) && (primeraHora < 23));
+      if ((primeraHora < 0) && (primeraHora > 23)) {
+        System.out.println("Hora incorrecta (0 - 23), intentelo de nuevo.");
+      }
+    } while ((primeraHora < 0) && (primeraHora > 23));
 
-    System.out.print("\033[1mPor favor, Introduzca la segunda hora: \033[0m");
+    System.out.println("\033[1mPor favor, Introduzca el segundo día (Letras) y la hora(0-23).\033[0m");
     do {
-      System.out.println("\033[4mDía: \033[0m");
-      String dia = s.next();
-      boolean correcto = false;
-      switch (dia.toUpperCase()){
-        case "LUNES": segundoDia = 1; correcto = true; break;
-        case "MARTES": segundoDia = 2; correcto = true; break;
+      System.out.print("\033[4mDía:\033[0m ");
+      segDia = s.next();
+      correcto = false;
+      switch (segDia.toUpperCase()){
+        case "LUNES": segundoDia = 1; segDia = "Lunes"; correcto = true; break;
+        case "MARTES": segundoDia = 2; segDia = "Martes"; correcto = true; break;
         case "MIÉRCOLES":
-        case "MIERCOLES": segundoDia = 3; correcto = true; break;
-        case "JUEVES": segundoDia = 4; correcto = true; break;
-        case "VIERNES": segundoDia = 5; correcto = true; break;
-        case "SABADO": segundoDia = 6; correcto = true; break;
-        case "DOMINGO": segundoDia = 7; correcto = true; break;
+        case "MIERCOLES": segundoDia = 3; segDia = "Miércoles"; correcto = true; break;
+        case "JUEVES": segundoDia = 4; segDia = "Jueves"; correcto = true; break;
+        case "VIERNES": segundoDia = 5; segDia = "Viernes"; correcto = true; break;
+        case "SABADO": segundoDia = 6; segDia = "Sabado"; correcto = true; break;
+        case "DOMINGO": segundoDia = 7; segDia = "Domingo"; correcto = true; break;
+      }
+      if (!correcto) { System.out.println("Día incorrecto, intentelo de nuevo."); }
+      else if (segundoDia < primerDia) {
+        System.out.println("El segundo día no puede ser anterior al primero. Intentelo de nuevo.");
+        correcto = false;
       }
     } while (!correcto);
-    System.out.println("\033[4mHora: \033[0m");
-    int segundaHora = s.nextInt();
+    do {
+      System.out.print("\033[4mHora:\033[0m ");
+      segundaHora = s.nextInt();
+      if ((segundaHora < 0) && (segundaHora > 23)) {
+        System.out.println("Hora incorrecta (0 - 23), intentelo de nuevo.");
+      }
+    } while ((segundaHora < 0) && (segundaHora > 23));
 
-    System.out.print("Entre las " + primeraHora + ":00h del " + primerDia + " y las " + segundaHora
-    + ":00h del " + segundoDia + " hay ");
+    System.out.println("Entre las " + primeraHora + ":00h del " + priDia + " y las " + segundaHora
+    + ":00h del " + segDia + " hay " + (((segundoDia - primerDia)*24) + (segundaHora - primeraHora))
+    + " horas.");
 
+    
 
   }
 }
