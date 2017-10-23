c98db2ee1b65083877ee52cb68ad5c7707047bc0
alumno-dam <guillermogarciaf@gmail.com>
2017-10-23 09:09:45 +0200

    Añadido Ejercicio 26 Tema 5

--- EjerciciosTema5/Ejercicio26.java
+++ EjerciciosTema5/Ejercicio26.java
@@ -19,14 +19,43 @@ public class Ejercicio26 {
     System.out.print("\033[1mIntroduzca un número: \033[0m");
 
     long numero = s.nextLong();
+    long temp = 0;
+    int longitud = 0;
+    int posicion = 1;
+    int veces = 0;
 
-    System.out.printf("El número \033[4m%d\033[0m al revés es \033[7m", numero);
+    System.out.print("\033[1mIntroduzca el dígito a buscar: \033[0m");
+    int digito = s.nextInt();
 
-    do {
-      System.out.printf("%d",(numero % 10));
+    System.out.print("En el ńumero \033[1m" + numero + "\033[0m el dígito " + digito);
+
+    while (numero > 0) {
+      if (digito == (numero % 10)){ veces++; }
+      temp = (temp * 10) + (numero % 10);
       numero = numero / 10;
-    } while (numero > 10);
-    System.out.printf("%d\033[0m.\n",numero);
+      longitud++;
+    }
+
+    if (veces == 0){
+      System.out.print(" no aparece ninguna vez.");
+    } else {
+      if (veces == 1){
+        System.out.print(" aparece en la posición ");
+      } else {
+        System.out.print(" aparece en las posiciones ");
+      }
+      while (temp > 0) {
+        if ((digito == (temp % 10)) && (veces > 0)){
+          System.out.print(posicion);
+          veces--;
+        } else {
+          temp = temp / 10;
+        }
+        posicion++;
+      }
+    }
+
+    System.out.println(".");
 
   }
 }
