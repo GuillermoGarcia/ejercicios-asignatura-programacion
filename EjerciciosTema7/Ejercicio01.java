02da89d10f513b6eb4dd0f51b678b6587a3f8b65
alumno-dam <guillermogarciaf@gmail.com>
2017-11-16 14:16:40 +0100

    Añadido Ejercicio 1 Tema 7

--- EjerciciosTema7/Ejercicio01.java
+++ EjerciciosTema7/Ejercicio01.java
@@ -1,5 +1,5 @@
 /*
- * Solución Ejercicio 2 Tema 7
+ * Solución Ejercicio 1 Tema 7
  * 
  * Define un num de 10 caracteres con nombre simbolo y asigna valores a los elementos según la
  * tabla que se muestra a continuación. Muestra el contenido de todos los elementos del num. ¿Qué
