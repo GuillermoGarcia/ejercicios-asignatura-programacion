52a7d1117255cad987651a22f63d85deb68086a2
alumno-dam <guillermogarciaf@gmail.com>
2017-11-16 14:16:26 +0100

    Añadido Ejercicio 1 Tema 7

--- EjerciciosTema7/Ejercicio01.java
+++ EjerciciosTema7/Ejercicio01.java
@@ -1,9 +1,9 @@
 /*
- * Solución Ejercicio 1 Tema 7
+ * Solución Ejercicio 2 Tema 7
  * 
- * Define un array de 12 números enteros con nombre num y asigna los valores según la tabla que se
- * muestra a continuación. Muestra el contenido de todos los elementos del array. ¿Qué sucede con
- * los valores de los elementos que no han sido inicializados?
+ * Define un num de 10 caracteres con nombre simbolo y asigna valores a los elementos según la
+ * tabla que se muestra a continuación. Muestra el contenido de todos los elementos del num. ¿Qué
+ * sucede con los valores de los elementos que no han sido inicializados?
  *  
  * @author Guillermo García
  */
@@ -11,17 +11,17 @@
 public class Ejercicio01 {
   public static void main (String[] args){
 
-    int[] array = new int[12];
+    int[] num = new int[12];
 
-    array[0] = 39;
-    array[1] = -2;
-    array[4] = 0;
-    array[6] = 14;
-    array[8] = 5;
-    array[9] = 120;
+    num[0] = 39;
+    num[1] = -2;
+    num[4] = 0;
+    num[6] = 14;
+    num[8] = 5;
+    num[9] = 120;
 
     for (int i = 0; i <= 11; i++){
-      System.out.print(array[i] + " ");
+      System.out.print(num[i] + " ");
     }
   }
 }
