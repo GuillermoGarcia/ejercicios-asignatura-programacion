3f029444f6fbb47c8e600fcc7eae45f5d807c824
Guillermo Garcia <guillermogarciaf@gmail.com>
2018-01-10 13:03:26 +0100

    Añadido Ejercicios 17 Tema 8

---
+++ EjerciciosTema08/Ejercicio17.java
@@ -0,0 +1,30 @@
+/*
+ * Escribe un programa que pase de binario a decimal.
+ * 
+ * @author: Guillermo García
+ */
+
+import ejerciciostema8.Ejercicios1a14Tema8;
+
+
+public class Ejercicio17 {
+  
+  public static void main (String[] args) {
+
+    int numero = 454457;
+
+    System.out.println("El numero " + numero + " en binario es " + DecimalABinario(numero) + ".\n");
+
+  }
+
+  public static long DecimalABinario(int numero){
+
+    long binario = 0;
+    while (numero > 2) {
+      binario = ((long)numero % 2) + (binario * 10);
+      numero /= 2;
+    }
+    return Ejercicios1a14Tema8.voltea((binario * 10) + numero);
+  }
+}
+
