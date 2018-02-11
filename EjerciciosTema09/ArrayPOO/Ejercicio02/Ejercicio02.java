4476d0807fdf5ff8056f2db5ee5bcd9557f9707e
Guillermo García Fernández <guillermogarciaf@gmail.com>
2018-02-11 20:31:34 +0100

    Añadido Ejercicio 2 Tema 9 Arrays

--- EjerciciosTema09/ArrayPOO/Ejercicio02/Ejercicio02.java
+++ EjerciciosTema09/ArrayPOO/Ejercicio02/Ejercicio02.java
@@ -1,41 +1,29 @@
 
 /*
- * Solución Ejercicio 1 Tema 9 Arrays
+ * Solución Ejercicio 2 Tema 9 Arrays
  *
- * Utiliza la clase Gato para crear un array de cuatro gatos e introduce los datos de cada uno de
- * ellos mediante un bucle. Muestra a continuación los datos de todos los gatos utilizando también
- * un bucle.
+ * Cambia el programa anterior de tal forma que los datos de los gatos se introduzcan directamente
+ * en el código de la forma gatito[2].setColor(“marrón”) o bien mediante el constructor, de la forma
+ * gatito[3] = new Gato(“Garfield”, “naranja”, “macho”) . Muestra a continuación los datos de todos
+ * los gatos utilizando un bucle.
  *
  * @author Guillermo García
  */
 
-import java.util.Scanner;
 
-public class Ejercicio01 {
+public class Ejercicio02 {
   public static void main (String[] args){
 
     Gato[] gatos = new Gato[4];
-    Scanner s = new Scanner(System.in);
-    Sexo sexo;
 
-    for (int i = 0; i < 4; i++){
-      System.out.print("¿Sexo del gato?(Macho, Hembra): ");
-      switch (s.next().toUpperCase()){
-        case "HEMBRA": sexo = Sexo.HEMBRA; break;
-        default: sexo = Sexo.MACHO;
-      }
-      System.out.print("¿Raza del gato?: ");
-      String raza = s.next();
-      System.out.print("¿Color del gato?: ");
-      String color = s.next();
-      System.out.print("¿Número de patas del gato?: ");
-      int patas = s.nextInt();
-      System.out.print("¿Número de mamas del gato?: ");
-      int mamas = s.nextInt();
-      System.out.print("¿Nombre del gato?: ");
-      String nombre = s.next();
-      gatos[i] = new Gato(sexo, raza, color, patas, mamas, nombre);
-    }
+    gatos[0] = new Gato(Sexo.MACHO, "Siames", "", 4, 8, "Lotte");
+    gatos[1] = new Gato(Sexo.HEMBRA, "Persa", "Blanco", 4, 6, "");
+    gatos[2] = new Gato(Sexo.MACHO, "Bengala", "Naranja", 0, 8, "Misifu");
+    gatos[3] = new Gato(Sexo.HEMBRA, "Abisinio", "Manchas", 4, 0, "Nefertiti");
+    gatos[0].setColor("Marron");
+    gatos[1].setNombre("Daisy");
+    gatos[2].setNumeroPiernas(4);
+    gatos[3].setNumeroMamas(6);
     System.out.println("\n\n\n");
     for (int i = 0; i < 4; i++){
       System.out.println(gatos[i]);
